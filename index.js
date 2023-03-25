db3e224a460976a7183e531a1feb0b09251abd9c
MohammadFaysal731 <www.faysal731@gmail.com>
2023-03-25 11:52:49 +0600

    deploy cylic

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
+    // cyclic commit for deployment
     try {
         await client.connect();
         const projectCollection = client.db('my-portfolio').collection('projects');
